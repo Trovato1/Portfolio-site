864491d4d1f8a6d97695292820881aaaedae7344
Jonathan Trovato <168646827+Trovato1@users.noreply.github.com>
2025-08-07 10:08:15 -0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/icaroesr/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/jonathan-trovato-7b063b203/"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
